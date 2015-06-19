807afdf0211aabac9e3849fa552b0f5f267c57b3
Tormod Haugland <tormod.haugland@gmail.com>
2015-06-19 14:36:52 +0200

    Updated manifest

--- app/src/main/java/org/trmd/snapslide/MainActivity.java
+++ app/src/main/java/org/trmd/snapslide/MainActivity.java
@@ -1,17 +1,76 @@
 package org.trmd.snapslide;
 
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Button;
+
+import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends Activity {
+
+    /**
+     * Our magnificent front-camera
+     */
+    private Camera backCamera;
+
+    /**
+     * Our magnificent back-camera
+     */
+    private Camera frontCamera;
+
+    /**
+     * The view we display our camera information on
+     */
+    private SurfaceView surfaceViewCamera;
+
+    /**
+     * The surface holder of the surfaceView
+     */
+    private SurfaceHolder surfaceHolderCamera;
+
+    /**
+     * Our "take picture" button
+     */
+    private Button buttonCamera;
+
+    /**
+     * The button which enables or disables blitz
+     */
+    private Button buttonEnableBlitz;
+
+    /**
+     * Button taking us to the settings page
+     */
+    private Button buttonSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    /**
+     * Initialize everything
+     */
+    protected void initialize() {
+        // Set the window to support translucency
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
+
+        surfaceViewCamera = (SurfaceView) findViewById(R.id.cameraSurfaceView);
+        buttonCamera = (Button) findViewById(R.id.cameraButton);
+
+        surfaceHolderCamera = surfaceViewCamera.getHolder();
+
     }
 
     @Override
@@ -34,5 +93,30 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class HolderCallback implements SurfaceHolder.Callback{
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                frontCamera = Camera.open();
+                frontCamera.setDisplayOrientation(90);
+                frontCamera.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
     }
 }
